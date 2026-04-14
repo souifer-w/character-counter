bc667a80cb9eac7e01f3ed9a28c32405637ca5bc
soulaimane <s.souifer1@gmail.com>
2026-04-15 00:19:00 +0100

    update project

--- character.js
+++ character.js
@@ -1,7 +1,7 @@
 const text = document.querySelector(".text");
 const increase = document.querySelector(".increase");
 const circle = document.querySelector(".circle");
-let textIncrease = JSON.parse(localStorage.getItem("textIncrease")) || 0;
+let textIncrease = 0;
 let max = 280;
 text.addEventListener("keyup", (event) => {
   let progress = Math.floor((text.value.length / max) * 100);
@@ -25,7 +25,9 @@ text.addEventListener("keyup", (event) => {
     circle.classList.add("finish");
     increase.classList.add("is-done");
     circle.style.background = `conic-gradient(#5900ff  ${progress}%, #5900ff ${progress}%)`;
+  } else {
+    circle.classList.remove("finish");
+    increase.classList.remove("is-done");
+    increase.innerHTML = `${progress}%`;
   }
-
-  localStorage.setItem("textIncrease", JSON.stringify(textIncrease));
 });
